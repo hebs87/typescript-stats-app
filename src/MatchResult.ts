9a2c52ee7c39e9b25d5e5d61e9ed69360f94704f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-10 22:35:03 +0000

    Moved MatchResult enum into separate file to enable reuse

---
+++ src/MatchResult.ts
@@ -0,0 +1,7 @@
+// enum - used to indicate a set of closely related results - stores either numbers or strings and accessed like objects
+// These can also be referenced as types, e.g. return value of a function
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 // Read the file and convert to 2 dimensional array - first split each new line, then split new line at commas
 const reader = new CsvFileReader('football.csv');
 reader.read();
 const matches = reader.data;
-
-// enum - used to indicate a set of closely related results - stores either numbers or strings and accessed like objects
-// These can also be referenced as types, e.g. return value of a function
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
 
 // Count number of Man Utd wins
 let manUnitedWins = 0;
