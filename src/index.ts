59bdeff1075b8bffec55f25694ae7d0749b92b94
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-10 01:32:36 +0000

    Added functionality to report on total Man United wins

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,14 @@ const matches = fs
     return row.split(',');
   });
 
-console.log(matches);
+// Count number of Man Utd wins
+let manUnitedWins = 0;
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
