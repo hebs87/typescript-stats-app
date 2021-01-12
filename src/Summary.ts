85366f59c57a4bbed6b4969e32f20ab41e8951eb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-12 23:16:06 +0000

    Added buildAndPrintReport method in Summary class to coordinate analyzer and output target classes

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,5 +1,14 @@
 import { Analyzer, OutputTarget } from "./interfaces";
+import { MatchData } from "./MatchData";
 
 export class Summary {
+  // We get the analyzer and the output target and call both relevant methods from them
+  // This class doesn't really have any functionality as such, it just holds references to other classes and brings
+  // their methods together - it is more of a coordinator
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {};
+
+  buildAndPrintReport = (matches: MatchData[]): void => {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  };
 }
